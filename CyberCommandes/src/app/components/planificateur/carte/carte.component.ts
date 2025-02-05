0b836c35297540c050fb56a3aa5f6c748b8cc7c0
Kalid Moussa Mohamed <kalidmoussamohamed@gmail.com>
2025-02-05 17:01:15 +0100

    Khali

---
+++ CyberCommandes/src/app/components/planificateur/carte/carte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte',
+  imports: [],
+  templateUrl: './carte.component.html',
+  styleUrl: './carte.component.css'
+})
+export class CarteComponent {
+
+}
