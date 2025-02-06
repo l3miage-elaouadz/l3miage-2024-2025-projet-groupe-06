7ec83c0b742baa6856b85c77cb28e5e95efcb78f
l3miage-dossos <siaka.dosso@etu.univ-grenoble-alpes.fr>
2025-02-06 16:46:21 +0100

    Update cartedesuivie.component.ts

--- CyberCommandes/src/app/components/planificateur/cartedesuivie/cartedesuivie.component.ts
+++ CyberCommandes/src/app/components/planificateur/cartedesuivie/cartedesuivie.component.ts
@@ -1,13 +1,10 @@
-import { Component, computed, effect, input, model, output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import { Icon, icon, LatLng, latLng, Layer, MapOptions, marker, Marker, polyline, Polyline, tileLayer } from 'leaflet';
-import { GeoServiceService } from '../services/geo-service.service'; 
+ 
 
 @Component({
   selector: 'app-cartedesuivie',
   imports: [
-    LeafletModule,
     FormsModule,
   ],
   templateUrl: './cartedesuivie.component.html',
@@ -15,37 +12,7 @@ import { GeoServiceService } from '../services/geo-service.service';
 })
 export class CartedesuivieComponent {
 
-  readonly latitude = model<number>(45.166672);
-  readonly longitude = model<number>(5.71667);
-  readonly zoom = model<number>(12);
-
-
-  clickOnMap = output<LatLng>();
-
-  readonly center = computed(() => latLng(this.latitude(), this.longitude()));
-
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
-  };
-
-  address: string = '';
-
-  constructor(private geoService: GeoServiceService) { }
-
   
-
-
-  async onMapClick(latlng: LatLng): Promise<void> {
-    this.clickOnMap.emit(latlng);
-    this.latitude.set(latlng.lat);
-    this.longitude.set(latlng.lng);
-
-    // Appel du service pour obtenir l'adresse
-    this.address = await this.geoService.reverseGeocode(latlng);
   }
 
 }
